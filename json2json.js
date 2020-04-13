d863ea0aa11b077ff0b572105dd830a2c0196f26
zhangweis <cdzhangw@gmail.com>
2020-04-13 21:05:00 +0800

    Update json2json.js
    handle exception.

--- json2json.js
+++ json2json.js
@@ -1,7 +1,12 @@
 const getStdin = require('get-stdin');
 module.exports = async function (func) {
+    try {
     var input = JSON.parse(await getStdin()); 
     var output = await func(input); 
     if (output==undefined) return;  
     console.log(JSON.stringify(output));
+    }catch(e) {
+        console.error(e.stack);
+        process.exit(1);
+    }
 }
