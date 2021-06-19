309faaff5f7684b7640c6f9cb44bfc44c6a6f167
zhangweis <cdzhangw@gmail.com>
2021-06-19 12:04:37 +0800

    support default export

--- bin/json2json.js
+++ bin/json2json.js
@@ -2,5 +2,6 @@
 const json2Json = require('../json2json.js');                               
 
     var func = require(process.cwd()+'/'+process.argv[2]);
+    func=func.default||func;
     process.argv = process.argv.slice(0,1).concat(process.argv.slice(2));
     json2Json(func);
